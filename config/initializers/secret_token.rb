68f205c30a1c81e0f66b65e051aac13b80a9fa2c
diane.uwacu <diane.uwacu@eagles.oc.edu>
2013-03-23 12:07:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartWordsApp::Application.config.secret_token = '0bbd33d959898bacea7620d2b26634a52abf580132b5afe72943b6c9fc230b623843507d624d8ac515a0d956161a8c19d4526210942dc338661f7a4ffe590d39'
